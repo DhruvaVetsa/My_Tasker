c654324635b7ee88ab54d2924c9784dcdcf11c26
DhruvaVetsa12ABC <95221871+DhruvaVetsa12ABC@users.noreply.github.com>
2022-05-29 08:58:01 +0530

    Final Commit

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,4 @@
 let mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/task');
-// mongoose.connect('mongodb+srv://DhruvaV-123:Deens@2021@cluster0.yqf1n.mongodb.net/myTasker?retryWrites=true&w=majority');
-mongoose.connect('mongodb+srv://Chandana:cvml462ccl4*@mytasker.wknnc.mongodb.net/myTasker0022?retryWrites=true&w=majority');
-
 let schema = mongoose.Schema(
   {
     content : {
